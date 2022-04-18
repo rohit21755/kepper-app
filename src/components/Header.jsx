fc4c56cf877bf5d2b048a5ed7aa7da384a28cd87
rohit21755 <77154384+rohit21755@users.noreply.github.com>
2022-04-19 02:46:55 +0530

    Add files via upload

---
+++ src/components/Header.jsx
@@ -0,0 +1,13 @@
+import React from "react";
+import LightbulbIcon from '@mui/icons-material/Lightbulb';
+
+function Header() {
+  return (
+    <header>
+      <h1>
+      <LightbulbIcon /> Keeper</h1>
+    </header>
+  );
+}
+
+export default Header;
